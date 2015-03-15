9dbbf732200e0bf03b40da8d2c15c9c232fbcc9a
Sarah Christ <schrist810@gmail.com>
2015-03-15 12:55:43 -0500

    header branding

--- wp-content/themes/sanctuary/header.php
+++ wp-content/themes/sanctuary/header.php
@@ -23,11 +23,11 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h1>Sanctuary</h1>
+		</div>
+			<div class="site-description">
+			<h2>Eco-Retreat for Professional Artists</h2>
 		</div><!-- .site-branding -->
-<h1>Sanctuary</h1>
-<p>Eco-Retreat for Professional Artists</p>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'sanctuary' ); ?></button>
 
